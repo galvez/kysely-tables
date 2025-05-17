f3fb24c2eec110afa6640eea76b0ec89335ecd7d
Jonas Galvez <jonasgalvez@gmail.com>
2025-05-17 05:31:54 -0300

    wip runner

--- src/example/database.ts
+++ src/example/database.ts
@@ -23,6 +23,7 @@ export interface UsersTable {
   fname: Sized<string, 100> | null
   email: Unique<Sized<string, 255>>
   passwordHash: Text<string>
+  role: Default<string, "'member'">
   createdAt: Default<Date, 'now()'>
   updatedAt: Default<Date, 'now()'>
 }
